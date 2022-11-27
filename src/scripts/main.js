c4f18cb5150b7e2986e7bf16bfa9914002b297ec
Hecdin Farias <contact@hecdin.com>
2022-11-27 19:42:32 -0400

    Update v1.1

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+// ONLOAD
+const $btnTheme = document.getElementById("id_btnTheme");
+$btnTheme.addEventListener("click", changeTheme);
+$btnTheme.textContent = "Theme";
+
+const $btnReturn = document.getElementById("id_btnReturn");
+$btnReturn.addEventListener("click", toReturn);
+$btnReturn.textContent = "<";
+
+if (localStorage.db_theme != null) {
+  document.body.dataset.theme = localStorage.getItem('db_theme')
+}
+
+
+// CHANGE THEME
+function changeTheme() {
+  let elem = document.body.dataset;
+  
+  (elem.theme === "normal")
+  ? elem.theme = "dark"
+  : elem.theme = "normal";
+  
+  localStorage.db_theme = elem.theme;
+}
+
+
+// TO RETURN
+function toReturn() {
+  location.pathname = '/AboutBlank/';
+}
